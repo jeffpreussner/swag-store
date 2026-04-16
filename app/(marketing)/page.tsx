d8eb002c5d01bfa171d3d5941b079d8600b77737
Jeff Preussner <jeff.preussner@velir.com>
2026-04-16 10:54:43 -0400

    Comment captured in readme removing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,8 +14,8 @@ export const metadata: Metadata = {
   description:
     "Acme Swag Homepage is the place to find the latest and greatest swaggy stuff!",
 };
+
 export default async function MarketingPage() {
-  // both promises can be awaited in parallel
   const promoPromise = fetchActivePromo();
   const featuredPromise = fetchProducts(true);
 
