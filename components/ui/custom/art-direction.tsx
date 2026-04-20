1dcaf482db0ff3af0f393c8f0ca4fa6fdbcf9803
Jeff Preussner <jeff.preussner@velir.com>
2026-04-20 13:35:13 -0400

    Format ArtDirection component and next.config.ts
    Apply consistent indentation and spacing across the ArtDirection component and image configuration settings to improve code readability.

--- components/ui/custom/art-direction.tsx
+++ components/ui/custom/art-direction.tsx
@@ -2,37 +2,44 @@ import dheroImg from "@/public/img/1200x400-grayscale.jpg";
 import mheroImg from "@/public/img/750x750-grayscale.jpg";
 import { getImageProps } from "next/image";
 
-export function ArtDirection(){
-    const {props: {srcSet: desktopSrcset}} =  getImageProps({
-        alt:"Store Hero Image",
-        sizes: "(min-width: 1024px) 1024px, 100vw",
-        src: dheroImg,
-        width: 1200,
-        height: 400,
-        quality: 75,
-        placeholder: "blur",
-    })
+export function ArtDirection() {
+  const {
+    props: { srcSet: desktopSrcset },
+  } = getImageProps({
+    alt: "Store Hero Image",
+    sizes: "(min-width: 1024px) 1024px, 100vw",
+    src: dheroImg,
+    width: 1200,
+    height: 400,
+    quality: 75,
+    placeholder: "blur",
+  });
 
-    const{props: {srcSet: mobileSrcset, ...rest}} =  getImageProps({
-        alt:"Store Hero Image",
-        sizes: "100vw",
-        src: mheroImg,
-        width: 750,
-        height: 750,
-        quality: 75,
-        placeholder: "blur",
-    })
+  const {
+    props: { srcSet: mobileSrcset, ...rest },
+  } = getImageProps({
+    alt: "Store Hero Image",
+    sizes: "100vw",
+    src: mheroImg,
+    width: 750,
+    height: 750,
+    quality: 75,
+    placeholder: "blur",
+  });
 
   return (
     <picture>
-        <source media="(min-width: 470px) and (max-width: 767px)" srcSet={mobileSrcset} />
-        <source media="(min-width: 768px)" srcSet={desktopSrcset} />
-        <img
+      <source
+        media="(min-width: 470px) and (max-width: 767px)"
+        srcSet={mobileSrcset}
+      />
+      <source media="(min-width: 768px)" srcSet={desktopSrcset} />
+      <img
         {...rest}
         fetchPriority="high"
         className="object-cover w-full h-full"
         loading="eager"
-        />
+      />
     </picture>
- )
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
     ],
     deviceSizes: [640, 768, 1024, 1280, 1536, 1920],
-    imageSizes: [ 40, 300, 750],
+    imageSizes: [40, 300, 750],
     qualities: [50, 75],
     formats: ["image/avif", "image/webp"],
   },
